4370e9a0fb1627d5a0fe85cdeac767e81125bc7f
Cody <c.jmiller17@yahoo.com>
2024-06-24 15:03:11 -0400

    added icons

--- src/About.jsx
+++ src/About.jsx
@@ -1,12 +1,53 @@
-
 import * as React from "react";
+import { IconButton, Link, Card, CardBody, Text } from "@chakra-ui/react";
+import { MdEmail } from "react-icons/md";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa6";
+
+
+
 
 function About() {
   return (
-    <div className="p-5">
-      <h1>About Page</h1>
-    </div>
-  )
+    <Card>
+      <CardBody>
+        <Text>Here are links to my contact information:</Text>
+
+        <IconButton
+          isRound={true}
+          variant="solid"
+          colorScheme="teal"
+          aria-label="Done"
+          fontSize="20px"
+          icon={<MdEmail />}
+          as={Link}
+          to=""
+        />
+
+        <IconButton
+          isRound={true}
+          variant="solid"
+          colorScheme="teal"
+          aria-label="Done"
+          fontSize="20px"
+          icon={<FaLinkedin />}
+          as={Link}
+          to=""
+        />
+
+        <IconButton
+          isRound={true}
+          variant="solid"
+          colorScheme="teal"
+          aria-label="Done"
+          fontSize="20px"
+          icon={<FaGithub />}
+          as={Link}
+          to=""
+        />
+      </CardBody>
+    </Card>
+  );
 }
 
 export default About
