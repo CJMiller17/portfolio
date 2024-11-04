8f0ff21e11994965656adee62b63046b1daa8319
Cody Miller <C.JMiller17@yahoo.com>
2024-11-04 13:11:40 -0500

    Added video thumbnails

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,7 +1,10 @@
-import React from "react";
-import { SimpleGrid, Card, Image, Box, HStack, Badge, Stack, Button, Heading, Text, CardBody, CardFooter, Wrap } from "@chakra-ui/react";
-
-// import "./App.css"
+import React, { useState } from "react";
+import { SimpleGrid, Card, Image, Modal, ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter, HStack, Badge, Stack, Button, Heading, Text, CardBody, CardFooter, Wrap } from "@chakra-ui/react";
 
 import Birding from "./assets/Birding.png";
 import StoryGen from "./assets/Story.png"
@@ -13,102 +16,138 @@ import Restaurant from "./assets/Food.png";
 import ToDoList from "./assets/ToDo.png";
 import Social from "./assets/Social.png";
 import Prince from "./assets/Prince.png";
+import GeoDash from "./assets/GeoDash.png";
+import GeoDashDemo from "./assets/GeoDashDemo.mov";
+
+
 // import Birding from "./assets/Birding_screenshot.png";
 
 
 export default function ProjectsCarousel() {
+  const [isModalOpen, setIsModalOpen] = useState(null)
+  const [hovered, setHovered] = useState(null)
 
   const elevatedCards = [
     {
       title: "Adventure Game",
       text: "Developed an immersive adventure game with a seamlessly integrated front-end and back-end, showcasing proficiency in full-stack development. This project honed my skills in creating interactive, user-driven experiences with real-time data management.",
       image: Prince,
-      link: "https://the-little-prince-returns.vercel.app/",
+      video: "",
+      liveLink: "https://the-little-prince-returns.vercel.app/",
+      githubLink: "https://github.com/CJMiller17/little-prince-client",
     },
     {
       title: "Birding Website",
       text: "Created a visually stunning birding website featuring multiple navigable pages and clickable images, emphasizing aesthetic design and user experience. This project enhanced my understanding of front-end development and responsive design principles.",
       image: Birding,
-      link: "https://cjmiller17.github.io/Birding/",
+      liveLink: "https://cjmiller17.github.io/Birding/",
+      githubLink: "https://github.com/CJMiller17/Birding",
     },
     {
       title: "Random Story Generator",
       text: "Designed a captivating random story generator inspired by the children's book 'The Gruffalo,' demonstrating creativity in algorithmic storytelling. This project improved my ability to implement logic for dynamic content generation.",
       image: StoryGen,
-      link: "https://cjmiller17.github.io/Silly-Story/",
+      liveLink: "https://cjmiller17.github.io/Silly-Story/",
+      githubLink: "https://github.com/CJMiller17/Silly-Story",
     },
     {
       title: "GeoDash World",
       text: "Designed a city exploration learning tool that can be customized for any city in the world.",
-      image: Clock,
-      link: "https://geodash-world-client-development.onrender.com/",
+      image: GeoDash,
+      video: GeoDashDemo,
+      liveLink: "https://geodash-world-client-development.onrender.com/",
+      githubLink: "https://github.com/chingu-voyages/v51-tier3-team-34",
     },
 
     // {
     //   title: "Restaurant",
     //   text: "Built a functional restaurant website with an intuitive search bar that retrieves data from an external API, showcasing my API integration skills. This project strengthened my proficiency in asynchronous JavaScript and data fetching techniques.",
     //   image: Restaurant,
-    //   link: "",
+    //   liveLink: "",
+    //   githubLink: "https://the-little-prince-returns.vercel.app/",
     // },
     // {
     //   title: "To Do List",
     //   text: "Developed an interactive to-do list application with editable input fields and dynamic color-changing status based on task deadlines, emphasizing task management and user interaction. This project advanced my knowledge of state management and real-time UI updates in JavaScript.",
     //   image: ToDoList,
-    //   link: "",
+    //   liveLink: "",
+    //   githubLink: "https://the-little-prince-returns.vercel.app/",
     // },
     // {
     //   title: "Clock",
     //   text: "The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters.",
     //   image: Clock,
-    //   link: "https://cjmiller17.github.io/Alarm-Clock/",
+    //   liveLink: "https://cjmiller17.github.io/Alarm-Clock/",
+    //   githubLink: "https://the-little-prince-returns.vercel.app/",
     // },
     // {
     //   title: "Mind Reader",
     //   text: "The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters.",
     //   image: MindReader,
-    //   link: "https://cjmiller17.github.io/mind-reader/",
+    //   liveLink: "https://cjmiller17.github.io/mind-reader/",
+    //   githubLink: "https://the-little-prince-returns.vercel.app/",
     // },
     // {
     //   title: "Weather App",
     //   text: "The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters.",
     //   image: Weather,
-    //   link: "https://cjmiller17.github.io/weather-app/",
+    //   liveLink: "https://cjmiller17.github.io/weather-app/",
+    //   githubLink: "https://the-little-prince-returns.vercel.app/",
     // },
     // {
     //   title: "Tic Tac Toe",
     //   text: "The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters.",
     //   image: TicTacToe,
-    //   link: "https://cjmiller17.github.io/terni-lapilli/",
+    //   liveLink: "https://cjmiller17.github.io/terni-lapilli/",
+    //   githubLink: "https://the-little-prince-returns.vercel.app/",
     // },
     // {
     //   title: "Social Media Site",
     //   text: "The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters.",
     //   image: Social,
-    //   link: "",
+    //   liveLink: "",
+    //   githubLink: "https://the-little-prince-returns.vercel.app/",
     // },
   ];
 
   return (
     <>
       <SimpleGrid columns={{ base: 1, sm: 1, md: 2, lg: 3 }} spacing=".5">
-        {elevatedCards.map((card) => (
+        {elevatedCards.map((card , index) => (
           <Card
-            key={card.title}
+            key={index}
             direction={{ base: "column", sm: "row" }} // Stack column for small screens, row for larger screens
             overflow="hidden"
             m="4"
             display="flex"
             flexDirection="column"
             height="93%"
+            onMouseEnter={() => setHovered(index)}
+            onMouseLeave={() => setHovered(null)}
           >
-            <Image
-              src={card.image}
-              alt={card.title}
-              objectFit="cover"
-              width="100%"
-              height="200px"
-              mt="1rem"
-            />
+            {hovered === index && card.video ? (
+              <video
+                src={card.video}
+                autoPlay
+                muted
+                loop
+                style={{
+                  width: "100%",
+                  height: "200px",
+                  objectFit: "cover"
+                }}
+              />
+            ) : (
+             <Image
+                src={card.image}
+                alt={card.title}
+                objectFit="cover"
+                width="100%"
+                height="200px"
+                mt="1rem"
+              />   
+            )
+            }
             <Stack p="4">
               <CardBody mt="-10" maxH="120px">
                 <Heading size="md" noOfLines={1}>
@@ -118,10 +157,33 @@ export default function ProjectsCarousel() {
                   fontSize=".9rem"
                   color="black"
                   maxHeight="90px"
-                  overflowY="scroll"
+                  noOfLines={4}
                 >
                   {card.text}
                 </Text>
+                <Button color="black" variant="link" onClick={() => setIsModalOpen(index)}>
+                  See More
+                </Button>
+
+                <Modal
+                  isOpen={isModalOpen === index}
+                  onClose={() => setIsModalOpen(null)}
+                >
+                  <ModalOverlay />
+                  <ModalContent>
+                    <ModalHeader>{card.title}</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                      <Text color="black">{card.text}</Text>
+                    </ModalBody>
+                    <ModalFooter>
+                      <Button onClick={() => setIsModalOpen(null)}>
+                        Close
+                      </Button>
+                    </ModalFooter>
+                  </ModalContent>
+                </Modal>
+
                 <Wrap mt="4" spacing="2" maxH="40px">
                   <Badge>React</Badge>
                   <Badge>Django</Badge>
@@ -131,10 +193,10 @@ export default function ProjectsCarousel() {
               </CardBody>
               <CardFooter mt="10">
                 <HStack mt="4" spacing="4" justifyContent="flex-end">
-                  <Button as="a" href={card.link} target="_blank">
+                  <Button as="a" href={card.liveLink} target="_blank">
                     Live
                   </Button>
-                  <Button as="a" href={card.link} target="_blank">
+                  <Button as="a" href={card.githubLink} target="_blank">
                     Code
                   </Button>
                 </HStack>
